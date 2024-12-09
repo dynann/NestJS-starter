0ca3ce4962ba5997c4d32964912158beb19723c5
dynann <dynannran@gmail.com>
2024-12-09 19:29:32 +0700

    handle existing validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,24 +7,35 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { User } from './users/entities/user.entity';
 import * as dotenv from 'dotenv';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 dotenv.config();
 
 @Module({
   imports: [
     NinjasModule,
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: process.env.NODE_ENV === 'local',
+      }),
     }),
     UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true, //make this configuration available globally
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.local',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -12,20 +12,76 @@ export class UsersService {
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User | null> {
-    const user = this.userRepository.create(createUserDto);
-    return await this.userRepository.save(user);
+    try {
+      const users = await this.findAll();
+      users.map((user) => {
+        if (user.email === createUserDto.email) {
+          throw new HttpException(
+            'this user already exists',
+            HttpStatus.CONFLICT,
+          );
+        }
+      });
+      const user = this.userRepository.create(createUserDto);
+      return await this.userRepository.save(user);
+    } catch (err) {
+      if (err instanceof HttpException) {
+        throw err;
+      }
+      throw new Error(err);
+    }
   }
 
-  findOne(id: number): Promise<User | null> {
-    return this.userRepository.findOneBy({ id });
+  async findOne(id: number): Promise<User | null> {
+    try {
+      const user = await this.userRepository.findOneBy({ id });
+      if (!user) {
+        throw new HttpException('no user found', HttpStatus.NOT_FOUND);
+      }
+      return user;
+    } catch (err) {
+      if (err instanceof HttpException) {
+        throw err;
+      }
+      throw new Error(err);
+    }
   }
 
-  findAll(): Promise<User[]> {
-    return this.userRepository.find();
+  async findAll(): Promise<User[]> {
+    try {
+      const res = await this.userRepository.find();
+      if (!res) {
+        throw new HttpException(' data base is empty ', HttpStatus.NOT_FOUND);
+      }
+      return res;
+    } catch (err) {
+      if (err instanceof HttpException) {
+        throw err;
+      }
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    await this.userRepository.update(id, updateUserDto);
+    try {
+      const user = await this.userRepository.findOneBy({ id });
+      if (!user) {
+        throw new HttpException(
+          'this user does not exist',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      await this.userRepository.update(id, updateUserDto);
+      return {
+        message: 'update user successfully',
+        statusCode: HttpStatus.OK,
+      };
+    } catch (err) {
+      if (err instanceof HttpException) {
+        throw err;
+      }
+      throw new Error(err);
+    }
   }
 
   async remove(id: number): Promise<void> {
